2f1707abd73c57ecd37a4281e6369da5c047fa75
Jason Kaufmann <jasonkaufmann07@gmail.com>
2024-08-11 17:25:00 -0700

    update

--- test.py
+++ test.py
@@ -15,4 +15,4 @@ def random_number():
     return jsonify(number=number)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)  # Run on port 5001
